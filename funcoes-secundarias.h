a79de0ca1a1bfc30911abf80b48bbadf1b694235
Restyled.io <commits@restyled.io>
2023-01-05 11:43:12 +0000

    Restyled by clang-format

--- funcoes-secundarias.h
+++ funcoes-secundarias.h
@@ -1,11 +1,9 @@
 #ifndef FUNCOES_SECUNDARIAS_H
 #define FUNCOES_SECUNDARIAS_H
 
+#include "estruturas.h"
 #include <stdio.h>
 #include <time.h>
-#include "estruturas.h"
-
-
 
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
 * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
@@ -13,49 +11,45 @@
 * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
 * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
+void quicksort() {}
 
-void quicksort() {
+void imprimir_funcoes(Total_funcoes_mil *lista_funcoes) {
 
-
+  for (int i = 0; i < lista_funcoes->cont_funcoes; i++) {
+    printf("%s", lista_funcoes->todas_funcoes[i].mil_funcao);
+  }
 }
 
-void imprimir_funcoes(Total_funcoes_mil * lista_funcoes) {
+// recebe lista de militares, o seu estado 0 = OP e 1 = INOP
+void imprimir_lista_militares(Total_militares *lista_militares, int estado,
+                              int funcao) {
 
-    for (int i = 0; i < lista_funcoes->cont_funcoes; i++) {
-        printf("%s",lista_funcoes->todas_funcoes[i].mil_funcao);
+  printf("NIP\tNOME\n");
+  for (int i = 0; i < lista_militares->cont_militares; i++) {
+
+    if ((lista_militares->total_tripulantes[i]->estado == estado ||
+         lista_militares->total_tripulantes[i]->estado == -1) &&
+        (lista_militares->total_tripulantes[i]->funcao == funcao ||
+         lista_militares->total_tripulantes[i]->funcao == -1)) {
+      printf("\n%d\t%s", lista_militares->total_tripulantes[i]->nip,
+             lista_militares->total_tripulantes[i]->nome);
     }
+  }
 }
 
-//recebe lista de militares, o seu estado 0 = OP e 1 = INOP
-void imprimir_lista_militares (Total_militares * lista_militares, int estado, int funcao) {
+void imprimir_militar(Total_militares *lista_militares, int nip) {
 
-    printf("NIP\tNOME\n");
-    for (int i = 0; i < lista_militares->cont_militares; i++) {
+  for (int i = 0; i < lista_militares->cont_militares; i++) {
 
-        if ((lista_militares->total_tripulantes[i]->estado==estado || lista_militares->total_tripulantes[i]->estado== -1)&&
-                (lista_militares->total_tripulantes[i]->funcao==funcao|| lista_militares->total_tripulantes[i]->funcao==-1)) {
-            printf("\n%d\t%s", lista_militares->total_tripulantes[i]->nip, lista_militares->total_tripulantes[i]->nome);
+    if (lista_militares->total_tripulantes[i]->nip == nip) {
 
-        }
-
+      printf("%s \n\thoras de voo: %d\n\tnumero de missoes: %d\n",
+             lista_militares->total_tripulantes[i]->nome,
+             lista_militares->total_tripulantes[i]->horas_voo,
+             lista_militares->total_tripulantes[i]->missoes);
+      break;
     }
-
-
+  }
 }
 
-void imprimir_militar (Total_militares * lista_militares, int nip) {
-
-    for (int i = 0; i < lista_militares->cont_militares; i++) {
-
-        if (lista_militares->total_tripulantes[i]->nip==nip) {
-
-            printf("%s \n\thoras de voo: %d\n\tnumero de missoes: %d\n", lista_militares->total_tripulantes[i]->nome,
-                   lista_militares->total_tripulantes[i]->horas_voo, lista_militares->total_tripulantes[i]->missoes);
-            break;
-
-        }
-    }
-
-}
-
-#endif //FUNCOES_SECUNDARIAS_H
+#endif // FUNCOES_SECUNDARIAS_H
--- funcoes_principais.h
+++ funcoes_principais.h
@@ -1,11 +1,11 @@
 #ifndef FUNCOES_PRINCIPAIS_H
 #define FUNCOES_PRINCIPAIS_H
 
-#include <stdio.h>
-#include <time.h>
-#include <string.h>
 #include "estruturas.h"
 #include "funcoes-secundarias.h"
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
 
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
 * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
@@ -13,77 +13,78 @@
 * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
 * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
-void marcar_missao(Total_missoes * total_missoes, Total_tipos_missao * tipos_missao) {
+void marcar_missao(Total_missoes *total_missoes,
+                   Total_tipos_missao *tipos_missao) {
 
+  int escolha, data[3], data_final = 0;
+  printf("\n Escolha o tipo de missao: \n");
+  printf("teste");
 
-    int escolha,data[3], data_final=0;
-    printf("\n Escolha o tipo de missao: \n");
-    printf("teste");
+  // ciclo que imprime todos os tipos de missao carregados no SIGIT
+  for (int i = 1; i <= tipos_missao->cont_tipos_missao; i++) {
+    printf("%d - %s\n", i, tipos_missao->conj_tipos_missao[i].nome);
+  }
+  scanf("%d", &escolha);
 
-    //ciclo que imprime todos os tipos de missao carregados no SIGIT
-    for (int i = 1; i <= tipos_missao->cont_tipos_missao ; i++) {
-        printf("%d - %s\n", i, tipos_missao->conj_tipos_missao[i].nome);
-    }
-    scanf("%d",&escolha);
+  printf("Insira a data, no formato DD/MM/AAAA");
 
-    printf("Insira a data, no formato DD/MM/AAAA");
+  // programa faz a leitura da data no formato comum e transforma no formato
+  // esperado pelo programa AAAAMMDD
+  scanf("%d//%d//%d", &data[0], &data[1], &data[2]);
+  data_final = data[0] + data[1] * 100 + data[2] * 10000;
 
-    //programa faz a leitura da data no formato comum e transforma no formato esperado pelo programa AAAAMMDD
-    scanf("%d//%d//%d",&data[0], &data[1], &data[2]);
-    data_final = data[0] + data[1] *100 + data[2]*10000;
+  // insere-se a data no vetor das missoes no formato do programa AAAAMMDD
+  total_missoes->conj_missoes[total_missoes->cont_missoes].data = data_final;
 
-    //insere-se a data no vetor das missoes no formato do programa AAAAMMDD
-    total_missoes->conj_missoes[total_missoes->cont_missoes].data = data_final;
-
-    //criação da equipa
-
+  // criação da equipa
 }
 
-void listar_missoes (Total_missoes * lista_de_missoes) {
-    Total_militares todos_mil;
-    int cont;
-    printf("%d %s %s %s",cont, todos_mil.total_tripulantes[1]->nip,todos_mil.total_tripulantes[1]->nome,todos_mil.total_tripulantes[1]->funcao);
-
-
+void listar_missoes(Total_missoes *lista_de_missoes) {
+  Total_militares todos_mil;
+  int cont;
+  printf("%d %s %s %s", cont, todos_mil.total_tripulantes[1]->nip,
+         todos_mil.total_tripulantes[1]->nome,
+         todos_mil.total_tripulantes[1]->funcao);
 }
 
-void listar_tripulantes(Total_militares * lista_de_militares, Total_funcoes_mil * lista_funcoes_mil) {
+void listar_tripulantes(Total_militares *lista_de_militares,
+                        Total_funcoes_mil *lista_funcoes_mil) {
 
-    int escolha, escolha2;
-    printf("Selecione a opcao:\n1 - lista total de militares OP\n2 - lista total de militares INOP\n3 - lista por funcao OP\n4 - lista por funcao INOP\n6 - sair");
-    scanf("%d", &escolha);
+  int escolha, escolha2;
+  printf("Selecione a opcao:\n1 - lista total de militares OP\n2 - lista total "
+         "de militares INOP\n3 - lista por funcao OP\n4 - lista por funcao "
+         "INOP\n6 - sair");
+  scanf("%d", &escolha);
 
-    switch (escolha) {
+  switch (escolha) {
 
-    case 1:
-        imprimir_lista_militares(lista_de_militares,0,-1);
-        break;
-    case 2:
-        imprimir_lista_militares(lista_de_militares,1,-1);
-        break;
-    case 3:
-        imprimir_funcoes(lista_funcoes_mil);
-        printf("escolha a funcao");
-        scanf("&d", &escolha2);
-        imprimir_lista_militares(lista_de_militares,0,escolha2);
-        break;
-    case 4:
-        imprimir_funcoes(lista_funcoes_mil);
-        printf("escolha a funcao");
-        scanf("&d", &escolha2);
-        imprimir_lista_militares(lista_de_militares,0,escolha2);
-        break;
+  case 1:
+    imprimir_lista_militares(lista_de_militares, 0, -1);
+    break;
+  case 2:
+    imprimir_lista_militares(lista_de_militares, 1, -1);
+    break;
+  case 3:
+    imprimir_funcoes(lista_funcoes_mil);
+    printf("escolha a funcao");
+    scanf("&d", &escolha2);
+    imprimir_lista_militares(lista_de_militares, 0, escolha2);
+    break;
+  case 4:
+    imprimir_funcoes(lista_funcoes_mil);
+    printf("escolha a funcao");
+    scanf("&d", &escolha2);
+    imprimir_lista_militares(lista_de_militares, 0, escolha2);
+    break;
 
-    case 5:
-        imprimir_lista_militares(lista_de_militares,-1,-1);
-    case 6:
-        return;
+  case 5:
+    imprimir_lista_militares(lista_de_militares, -1, -1);
+  case 6:
+    return;
 
-    default:
-        printf("numero introduzido não faz parte do menu");
-    }
-
+  default:
+    printf("numero introduzido não faz parte do menu");
+  }
 }
 
-
-#endif //FUNCOES_PRINCIPAIS_H
+#endif // FUNCOES_PRINCIPAIS_H
