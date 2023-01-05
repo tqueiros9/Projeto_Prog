f28df6668285152e16590d6bcebf6127e223741f
tqueiros9 <80295858+tqueiros9@users.noreply.github.com>
2023-01-05 11:40:39 +0000

    teste github

--- funcoes-secundarias.h
+++ funcoes-secundarias.h
@@ -19,4 +19,43 @@ void quicksort(){
 
 }
 
+void imprimir_funcoes(Total_funcoes_mil * lista_funcoes){
+
+    for (int i = 0; i < lista_funcoes->cont_funcoes; i++) {
+        printf("%s",lista_funcoes->todas_funcoes[i].mil_funcao);
+    }
+}
+
+//recebe lista de militares, o seu estado 0 = OP e 1 = INOP
+void imprimir_lista_militares (Total_militares * lista_militares, int estado, int funcao){
+
+    printf("NIP\tNOME\n");
+    for (int i = 0; i < lista_militares->cont_militares; i++) {
+
+        if ((lista_militares->total_tripulantes[i]->estado==estado || lista_militares->total_tripulantes[i]->estado== -1)&&
+        (lista_militares->total_tripulantes[i]->funcao==funcao|| lista_militares->total_tripulantes[i]->funcao==-1)){
+            printf("\n%d\t%s", lista_militares->total_tripulantes[i]->nip, lista_militares->total_tripulantes[i]->nome);
+
+        }
+
+    }
+
+
+}
+
+void imprimir_militar (Total_militares * lista_militares, int nip){
+
+    for (int i = 0; i < lista_militares->cont_militares; i++) {
+
+        if (lista_militares->total_tripulantes[i]->nip==nip){
+
+            printf("%s \n\thoras de voo: %d\n\tnumero de missoes: %d\n", lista_militares->total_tripulantes[i]->nome,
+                   lista_militares->total_tripulantes[i]->horas_voo, lista_militares->total_tripulantes[i]->missoes);
+            break;
+
+        }
+    }
+    
+}
+
 #endif //FUNCOES_SECUNDARIAS_H
--- funcoes_principais.h
+++ funcoes_principais.h
@@ -3,6 +3,7 @@
 
 #include <stdio.h>
 #include <time.h>
+#include <string.h>
 #include "estruturas.h"
 #include "funcoes-secundarias.h"
 
@@ -38,12 +39,51 @@ void marcar_missao(Total_missoes * total_missoes, Total_tipos_missao * tipos_mis
 
 }
 
-void listar_missoes (){
+void listar_missoes (Total_missoes * lista_de_missoes){
     Total_militares todos_mil;
     int cont;
     printf("%d %s %s %s",cont, todos_mil.total_tripulantes[1]->nip,todos_mil.total_tripulantes[1]->nome,todos_mil.total_tripulantes[1]->funcao);
 
 
 }
+
+void listar_tripulantes(Total_militares * lista_de_militares, Total_funcoes_mil * lista_funcoes_mil){
+
+    int escolha, escolha2;
+    printf("Selecione a opcao:\n1 - lista total de militares OP\n2 - lista total de militares INOP\n3 - lista por funcao OP\n4 - lista por funcao INOP\n6 - sair");
+    scanf("%d", &escolha);
+
+    switch (escolha) {
+
+        case 1:
+            imprimir_lista_militares(lista_de_militares,0,-1);
+            break;
+        case 2:
+            imprimir_lista_militares(lista_de_militares,1,-1);
+            break;
+        case 3:
+            imprimir_funcoes(lista_funcoes_mil);
+            printf("escolha a funcao");
+            scanf("&d", &escolha2);
+            imprimir_lista_militares(lista_de_militares,0,escolha2);
+            break;
+        case 4:
+            imprimir_funcoes(lista_funcoes_mil);
+            printf("escolha a funcao");
+            scanf("&d", &escolha2);
+            imprimir_lista_militares(lista_de_militares,0,escolha2);
+            break;
+
+        case 5:
+            imprimir_lista_militares(lista_de_militares,-1,-1);
+        case 6:
+            return;
+
+        default:
+            printf("numero introduzido não faz parte do menu");
+    }
+
+}
+
 
 #endif //FUNCOES_PRINCIPAIS_H
