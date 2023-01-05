eee3edae7a3f164748c6e63f1a5015c0ba5541b4
Restyled.io <commits@restyled.io>
2023-01-05 16:59:41 +0000

    Restyled by clang-format

--- funcoes-secundarias.h
+++ funcoes-secundarias.h
@@ -1,11 +1,9 @@
 #ifndef FUNCOES_SECUNDARIAS_H
 #define FUNCOES_SECUNDARIAS_H
 
+#include "estruturas.h"
 #include <stdio.h>
 #include <time.h>
-#include "estruturas.h"
-
-
 
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
 * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
@@ -13,53 +11,56 @@
 * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
 * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
-void iniciar_valores(Total_funcoes_mil * a, Total_militares * b, Total_missoes * c, Total_tipos_missao * d) {
-    a->cont_funcoes=0;
-    b->cont_militares=0;
-    c->cont_missoes=0;
-    d->cont_tipos_missao=0;
+void iniciar_valores(Total_funcoes_mil *a, Total_militares *b, Total_missoes *c,
+                     Total_tipos_missao *d) {
+  a->cont_funcoes = 0;
+  b->cont_militares = 0;
+  c->cont_missoes = 0;
+  d->cont_tipos_missao = 0;
 }
 void imprimir_menu() {
-    printf("\n1-marcar missao\n2-listar missoes\n3-listar tripulantes\n4-atualizar estado de tripulantes\n5-sair");
+  printf("\n1-marcar missao\n2-listar missoes\n3-listar "
+         "tripulantes\n4-atualizar estado de tripulantes\n5-sair");
 }
 
+void imprimir_funcoes(Total_funcoes_mil *lista_funcoes) {
 
+  for (int i = 0; i < lista_funcoes->cont_funcoes; i++) {
+    printf("%s", lista_funcoes->todas_funcoes[i].mil_funcao);
+  }
+}
 
-void imprimir_funcoes(Total_funcoes_mil * lista_funcoes) {
+// recebe lista de militares, o seu estado 0 = OP e 1 = INOP
+void imprimir_lista_militares(Total_militares *lista_militares, int estado,
+                              int funcao) {
 
-    for (int i = 0; i < lista_funcoes->cont_funcoes; i++) {
-        printf("%s",lista_funcoes->todas_funcoes[i].mil_funcao);
+  printf("NIP\tNOME\n");
+
+  for (int i = 0; i < lista_militares->cont_militares; i++) {
+
+    if ((lista_militares->total_tripulantes[i]->estado == estado ||
+         lista_militares->total_tripulantes[i]->estado == -1) &&
+        (lista_militares->total_tripulantes[i]->funcao == funcao ||
+         lista_militares->total_tripulantes[i]->funcao == -1)) {
+      printf("\n%d\t%s", lista_militares->total_tripulantes[i]->nip,
+             lista_militares->total_tripulantes[i]->nome);
     }
+  }
 }
 
-//recebe lista de militares, o seu estado 0 = OP e 1 = INOP
-void imprimir_lista_militares (Total_militares * lista_militares, int estado, int funcao) {
+void imprimir_militar(Total_militares *lista_militares, int nip) {
 
-    printf("NIP\tNOME\n");
+  for (int i = 0; i < lista_militares->cont_militares; i++) {
 
-    for (int i = 0; i < lista_militares->cont_militares; i++) {
+    if (lista_militares->total_tripulantes[i]->nip == nip) {
 
-        if ((lista_militares->total_tripulantes[i]->estado==estado || lista_militares->total_tripulantes[i]->estado== -1)&&
-                (lista_militares->total_tripulantes[i]->funcao==funcao|| lista_militares->total_tripulantes[i]->funcao==-1)) {
-            printf("\n%d\t%s", lista_militares->total_tripulantes[i]->nip, lista_militares->total_tripulantes[i]->nome);
-
-        }
+      printf("%s \n\thoras de voo: %d\n\tnumero de missoes: %d\n",
+             lista_militares->total_tripulantes[i]->nome,
+             lista_militares->total_tripulantes[i]->horas_voo,
+             lista_militares->total_tripulantes[i]->missoes);
+      break;
     }
+  }
 }
 
-void imprimir_militar (Total_militares * lista_militares, int nip) {
-
-    for (int i = 0; i < lista_militares->cont_militares; i++) {
-
-        if (lista_militares->total_tripulantes[i]->nip==nip) {
-
-            printf("%s \n\thoras de voo: %d\n\tnumero de missoes: %d\n", lista_militares->total_tripulantes[i]->nome,
-                   lista_militares->total_tripulantes[i]->horas_voo, lista_militares->total_tripulantes[i]->missoes);
-            break;
-
-        }
-    }
-
-}
-
-#endif //FUNCOES_SECUNDARIAS_H
+#endif // FUNCOES_SECUNDARIAS_H
--- main.c
+++ main.c
@@ -3,153 +3,141 @@
 #include <time.h>
 
 #include "estruturas.h"
-#include "funcoes_principais.h"
 #include "funcoes-secundarias.h"
-
+#include "funcoes_principais.h"
 
 #define MAX_TRIP 100
 
-
-
-
-void print_trip(Tripulante * t) {
-    printf("\n--Trip--\n");
-    printf("nome: %s\n", t->nome);
-    printf("nip:%d\n", t->nip);
-    printf("--------------\n");
+void print_trip(Tripulante *t) {
+  printf("\n--Trip--\n");
+  printf("nome: %s\n", t->nome);
+  printf("nip:%d\n", t->nip);
+  printf("--------------\n");
 }
 
-void print_trip_file(FILE * fp) {
-    int n, trip_counter;
-    Tripulante t;
-    // fp é um apontador de FILE já aberto em modo que suporta leitura binária
+void print_trip_file(FILE *fp) {
+  int n, trip_counter;
+  Tripulante t;
+  // fp é um apontador de FILE já aberto em modo que suporta leitura binária
 
+  // rewind inicio do ficheiro
+  rewind(fp);
+  // o primeiro dado do ficheiro é um número inteiro com o número de tripulantes
+  fread(&n, sizeof(int), 1, fp);
 
-    //rewind inicio do ficheiro
-    rewind(fp);
-    // o primeiro dado do ficheiro é um número inteiro com o número de tripulantes
-    fread(&n, sizeof(int), 1, fp);
+  // posteriormente, seguem-se todos os tripulantes
+  // verificar que o número de tripulantes lidos é efetivamente o indicado
+  //     no número inteiro inicial
+  // rewind do ficheiro, para voltar ao inicio
+  // ler um inteiro do ficheiro
+  // ler todos os tripulantes, um a um
+  for (trip_counter = 0; fread(&t, sizeof(Tripulante), 1, fp) != 0;
+       trip_counter++) {
+    print_trip(&t);
+  }
+  if (n != trip_counter) {
+    printf("ERRO");
+  }
 
-
-    // posteriormente, seguem-se todos os tripulantes
-    // verificar que o número de tripulantes lidos é efetivamente o indicado
-    //     no número inteiro inicial
-    // rewind do ficheiro, para voltar ao inicio
-    // ler um inteiro do ficheiro
-    // ler todos os tripulantes, um a um
-    for (trip_counter = 0; fread(&t, sizeof (Tripulante), 1, fp) != 0 ; trip_counter++) {
-        print_trip(&t);
-
-    }
-    if (n != trip_counter) {
-        printf("ERRO");
-    }
-
-    // parar de ler tripulantes quando fread devolver 0
-    // comparar se o número de tripulantes total lido é igual ao inteiro lido do inicio do ficheiro
+  // parar de ler tripulantes quando fread devolver 0
+  // comparar se o número de tripulantes total lido é igual ao inteiro lido do
+  // inicio do ficheiro
 }
-
 
 int print_trip_idx(FILE *fp, int idx) {
-    Tripulante t;
-    rewind(fp);
-    // fp é um apontador de FILE já aberto em modo que suporta leitura binária
-    // idx é o índice do Tripulante que queremos ler
-    fseek(fp, sizeof (int) + sizeof (Tripulante) * idx, SEEK_SET);
+  Tripulante t;
+  rewind(fp);
+  // fp é um apontador de FILE já aberto em modo que suporta leitura binária
+  // idx é o índice do Tripulante que queremos ler
+  fseek(fp, sizeof(int) + sizeof(Tripulante) * idx, SEEK_SET);
 
-    fread(&t, sizeof (Tripulante), 1, fp);
-    print_trip(&t);
+  fread(&t, sizeof(Tripulante), 1, fp);
+  print_trip(&t);
 }
 
-
-void trip_file_swap(FILE * fp, int idx1, int idx2) {
-    // fp é um apontador de FILE já aberto em modo que suporta leitura e escrita binária
-    // idx1 e idx2 são índices de Tripulantes no ficheiro
-    // esta função troca os dois tripulantes
+void trip_file_swap(FILE *fp, int idx1, int idx2) {
+  // fp é um apontador de FILE já aberto em modo que suporta leitura e escrita
+  // binária idx1 e idx2 são índices de Tripulantes no ficheiro esta função
+  // troca os dois tripulantes
 }
-
 
 int main_demo_bin() {
-    Tripulante t1 = {"Saramago", 136787};
-    Tripulante t2 = {"Asimov", 136786};
-    Tripulante t3 = {"Hofstader", 136788};
-    char filename[] = "tripulantes.bd";
-    Tripulante tripulacao[MAX_TRIP];
-    tripulacao[0] = t1;
-    tripulacao[1] = t2;
-    tripulacao[2] = t3;
-    FILE * fp, *fp2;
-    // vamos escrever um tripulante no ficheiro binário
-    fp = fopen("tripulantes.db", "wb");
-    if(fp == NULL) {
-        printf("Abertura de ficheiro incorrecta.\n");
-        return 1;
-    }
-    else {
-        int n=3;
-        printf("Ficheiro aberto.");
-        fwrite(&n, sizeof(int), 1, fp);
-        fwrite(tripulacao, sizeof(Tripulante), n, fp);
-        fclose(fp);
-    }
-    // vamos ler esse tripulante do ficheiro binário
-    fp = fopen("tripulantes.db", "rb");
-    if(fp == NULL) {
-        printf("Abertura de ficheiro incorrecta.\n");
-        return 1;
-    }
-    else {
-        printf("Ficheiro aberto.");
-        print_trip_idx(fp, 2);
-        print_trip_file(fp);
-        fclose(fp);
-    }
+  Tripulante t1 = {"Saramago", 136787};
+  Tripulante t2 = {"Asimov", 136786};
+  Tripulante t3 = {"Hofstader", 136788};
+  char filename[] = "tripulantes.bd";
+  Tripulante tripulacao[MAX_TRIP];
+  tripulacao[0] = t1;
+  tripulacao[1] = t2;
+  tripulacao[2] = t3;
+  FILE *fp, *fp2;
+  // vamos escrever um tripulante no ficheiro binário
+  fp = fopen("tripulantes.db", "wb");
+  if (fp == NULL) {
+    printf("Abertura de ficheiro incorrecta.\n");
+    return 1;
+  } else {
+    int n = 3;
+    printf("Ficheiro aberto.");
+    fwrite(&n, sizeof(int), 1, fp);
+    fwrite(tripulacao, sizeof(Tripulante), n, fp);
+    fclose(fp);
+  }
+  // vamos ler esse tripulante do ficheiro binário
+  fp = fopen("tripulantes.db", "rb");
+  if (fp == NULL) {
+    printf("Abertura de ficheiro incorrecta.\n");
+    return 1;
+  } else {
+    printf("Ficheiro aberto.");
+    print_trip_idx(fp, 2);
+    print_trip_file(fp);
+    fclose(fp);
+  }
 }
-
 
 int main() {
 
-    int escolha_menu;
-    Total_tipos_missao todos_tipos_missoes;
-    Total_missoes todas_as_missoes;
-    Total_militares todos_os_militares;
-    Total_funcoes_mil todas_func_mil;
+  int escolha_menu;
+  Total_tipos_missao todos_tipos_missoes;
+  Total_missoes todas_as_missoes;
+  Total_militares todos_os_militares;
+  Total_funcoes_mil todas_func_mil;
 
-    iniciar_valores(&todas_func_mil, &todos_os_militares, &todas_as_missoes, &todos_tipos_missoes);
+  iniciar_valores(&todas_func_mil, &todos_os_militares, &todas_as_missoes,
+                  &todos_tipos_missoes);
 
-    //abrir ficheiro
+  // abrir ficheiro
 
-    //main_demo_txt();
-    main_demo_bin();
-    printf("git test");
-    //teste datas
+  // main_demo_txt();
+  main_demo_bin();
+  printf("git test");
+  // teste datas
 
+  imprimir_menu();
+  scanf("%d", &escolha_menu);
+  while (escolha_menu != 5) {
 
-    imprimir_menu();
-    scanf("%d",&escolha_menu);
-    while(escolha_menu!=5) {
+    switch (escolha_menu) {
 
-        switch (escolha_menu) {
-
-        case 1:
-            marcar_missao(&todas_as_missoes, &todos_tipos_missoes);
-            break;
-        case 2:
-            listar_missoes(&todas_as_missoes, &todos_tipos_missoes, &todas_func_mil);
-            break;
-        case 3:
-            listar_tripulantes(&todos_os_militares, &todas_func_mil);
-            break;
-        case 4:
-            //atualizar estado militares
-            break;
-        case 5:
-            return 0;
-        default:
-            printf("escolha invalida, nao existe essa opcao no menu");
-        }
-        imprimir_menu();
-        scanf("%d",&escolha_menu);
+    case 1:
+      marcar_missao(&todas_as_missoes, &todos_tipos_missoes);
+      break;
+    case 2:
+      listar_missoes(&todas_as_missoes, &todos_tipos_missoes, &todas_func_mil);
+      break;
+    case 3:
+      listar_tripulantes(&todos_os_militares, &todas_func_mil);
+      break;
+    case 4:
+      // atualizar estado militares
+      break;
+    case 5:
+      return 0;
+    default:
+      printf("escolha invalida, nao existe essa opcao no menu");
     }
-
+    imprimir_menu();
+    scanf("%d", &escolha_menu);
+  }
 }
