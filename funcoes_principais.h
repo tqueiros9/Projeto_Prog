d373a7718367ffc4513343fd4ab0748786df93c3
Restyled.io <commits@restyled.io>
2023-01-05 11:43:11 +0000

    Restyled by astyle

--- funcoes_principais.h
+++ funcoes_principais.h
@@ -13,7 +13,7 @@
 * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
 * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
-void marcar_missao(Total_missoes * total_missoes, Total_tipos_missao * tipos_missao){
+void marcar_missao(Total_missoes * total_missoes, Total_tipos_missao * tipos_missao) {
 
 
     int escolha,data[3], data_final=0;
@@ -39,7 +39,7 @@ void marcar_missao(Total_missoes * total_missoes, Total_tipos_missao * tipos_mis
 
 }
 
-void listar_missoes (Total_missoes * lista_de_missoes){
+void listar_missoes (Total_missoes * lista_de_missoes) {
     Total_militares todos_mil;
     int cont;
     printf("%d %s %s %s",cont, todos_mil.total_tripulantes[1]->nip,todos_mil.total_tripulantes[1]->nome,todos_mil.total_tripulantes[1]->funcao);
@@ -47,7 +47,7 @@ void listar_missoes (Total_missoes * lista_de_missoes){
 
 }
 
-void listar_tripulantes(Total_militares * lista_de_militares, Total_funcoes_mil * lista_funcoes_mil){
+void listar_tripulantes(Total_militares * lista_de_militares, Total_funcoes_mil * lista_funcoes_mil) {
 
     int escolha, escolha2;
     printf("Selecione a opcao:\n1 - lista total de militares OP\n2 - lista total de militares INOP\n3 - lista por funcao OP\n4 - lista por funcao INOP\n6 - sair");
@@ -55,32 +55,32 @@ void listar_tripulantes(Total_militares * lista_de_militares, Total_funcoes_mil 
 
     switch (escolha) {
 
-        case 1:
-            imprimir_lista_militares(lista_de_militares,0,-1);
-            break;
-        case 2:
-            imprimir_lista_militares(lista_de_militares,1,-1);
-            break;
-        case 3:
-            imprimir_funcoes(lista_funcoes_mil);
-            printf("escolha a funcao");
-            scanf("&d", &escolha2);
-            imprimir_lista_militares(lista_de_militares,0,escolha2);
-            break;
-        case 4:
-            imprimir_funcoes(lista_funcoes_mil);
-            printf("escolha a funcao");
-            scanf("&d", &escolha2);
-            imprimir_lista_militares(lista_de_militares,0,escolha2);
-            break;
+    case 1:
+        imprimir_lista_militares(lista_de_militares,0,-1);
+        break;
+    case 2:
+        imprimir_lista_militares(lista_de_militares,1,-1);
+        break;
+    case 3:
+        imprimir_funcoes(lista_funcoes_mil);
+        printf("escolha a funcao");
+        scanf("&d", &escolha2);
+        imprimir_lista_militares(lista_de_militares,0,escolha2);
+        break;
+    case 4:
+        imprimir_funcoes(lista_funcoes_mil);
+        printf("escolha a funcao");
+        scanf("&d", &escolha2);
+        imprimir_lista_militares(lista_de_militares,0,escolha2);
+        break;
 
-        case 5:
-            imprimir_lista_militares(lista_de_militares,-1,-1);
-        case 6:
-            return;
+    case 5:
+        imprimir_lista_militares(lista_de_militares,-1,-1);
+    case 6:
+        return;
 
-        default:
-            printf("numero introduzido não faz parte do menu");
+    default:
+        printf("numero introduzido não faz parte do menu");
     }
 
 }
