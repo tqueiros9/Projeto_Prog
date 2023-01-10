ab1cd0990b3f6b13131cc2fb9e0ae5b2ffc374c1
Restyled.io <commits@restyled.io>
2023-01-10 03:21:41 +0000

    Restyled by clang-format

--- main.c
+++ main.c
@@ -1,65 +1,54 @@
 #include <stdio.h>
 
 #include "estruturas.h"
-#include "funcoes_principais.h"
 #include "funcoes-secundarias.h"
-
+#include "funcoes_principais.h"
 
 int main() {
 
+  int escolha_menu;
+  Total_tipos_missao todos_tipos_missoes;
+  Total_missoes todasAsMissoes;
+  Total_militares todos_os_militares;
+  Total_funcoes_mil todas_func_mil;
 
+  iniciar_valores(&todas_func_mil, &todos_os_militares, &todasAsMissoes,
+                  &todos_tipos_missoes);
+  ler_dados_binario(&todos_os_militares, &todasAsMissoes);
 
-    int escolha_menu;
-    Total_tipos_missao todos_tipos_missoes;
-    Total_missoes todasAsMissoes;
-    Total_militares todos_os_militares;
-    Total_funcoes_mil todas_func_mil;
+  imprimir_menu();
+  scanf("%d", &escolha_menu);
+  while (escolha_menu != 5) {
 
-    iniciar_valores(&todas_func_mil, &todos_os_militares, &todasAsMissoes, &todos_tipos_missoes);
-    ler_dados_binario(&todos_os_militares,&todasAsMissoes);
+    switch (escolha_menu) {
 
-    imprimir_menu();
-    scanf("%d",&escolha_menu);
-    while(escolha_menu!=5) {
+    case 1:
+      marcar_missao(&todasAsMissoes, &todos_tipos_missoes, &todos_os_militares,
+                    &todas_func_mil);
+      break;
+    case 2:
+      listar_missoes(&todasAsMissoes, &todos_tipos_missoes, &todas_func_mil);
+      break;
+    case 3:
+      listar_tripulantes(&todos_os_militares, &todas_func_mil);
+      break;
+    case 4:
+      // atualizar estado militares
+      atualizar_estado(&todos_os_militares);
 
-        switch (escolha_menu) {
+      break;
+    case 5:
+      // adicionar ficheiro à BD
+      break;
+    case 6:
+      guardar_dados_binario(&todos_os_militares, &todasAsMissoes);
+      return 0;
 
-        case 1:
-            marcar_missao(&todasAsMissoes, &todos_tipos_missoes, &todos_os_militares, &todas_func_mil);
-            break;
-        case 2:
-            listar_missoes(&todasAsMissoes, &todos_tipos_missoes, &todas_func_mil);
-            break;
-        case 3:
-            listar_tripulantes(&todos_os_militares, &todas_func_mil);
-            break;
-        case 4:
-            //atualizar estado militares
-            atualizar_estado(&todos_os_militares);
-
-            break;
-        case 5:
-            //adicionar ficheiro à BD
-            break;
-        case 6:
-            guardar_dados_binario(&todos_os_militares, &todasAsMissoes);
-            return 0;
-
-        default:
-            printf("escolha invalida, nao existe essa opcao no menu");
-        }
-
-        imprimir_menu();
-        scanf("%d",&escolha_menu);
+    default:
+      printf("escolha invalida, nao existe essa opcao no menu");
     }
 
-
-
-
-
-
-
-
+    imprimir_menu();
+    scanf("%d", &escolha_menu);
+  }
 }
-
-
